b4b8d6caaee10965402e619fa18d8980c7b7df12
Uzua Timothy <timchosen@gmail.com>
2021-01-20 00:05:43 +0100

    fixed it

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute, Router, NavigationEnd} from '@angular/router'; // <-- do not forget to import
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  private fragment: string;
   public isCollapsed = true;
 
-  constructor() { }
-
+  constructor(private route: ActivatedRoute, private router: Router) {
+    router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(true); }
+        }
+      }
+    });
+   }
   ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => { this.fragment = fragment; });
+  }
+
+  ngAfterViewInit(): void {
+    try {
+      document.querySelector('#' + this.fragment).scrollIntoView();
+    } catch (e) { }
   }
 
 }
