d4e547a40336321ca786a6aa2e467350c45e8596
Uzua Timothy <timchosen@gmail.com>
2021-02-06 00:44:01 +0100

    fixed erors

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 user:any;
+isLoading = false
 showApiKeys = false;
 settingsActive = "";
 aboutActive = "active";
